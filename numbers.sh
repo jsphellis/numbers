5eeedd288df585dc556435bd5e73b49e56f93688
Joseph Ellis <joellis@chapman.edu>
2020-11-29 15:22:11 +0800

    Adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# numbers.sh
+# Joseph Ellis
+echo "Enter a positive integer: "
+read NUMBER 
+N=1
+while [ "$N" -lt "$NUMBER" ]
+do 
+	if [ $((N%2)) -eq 0 ]
+	then 
+		echo $N "even"
+	else
+		echo $N "odd"
+	fi
+	N=$[$N+1]
+done
+echo "Thank you"
